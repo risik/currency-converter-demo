da475b5c6c62f21621f38e3378ae5367dd23f6b1
Sergei Borisov <sergeybor@gmail.com>
2015-02-17 21:17:23 +0300

    Add just generated application

---
+++ SimpleConverter/SimpleConverter/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  SimpleConverter
+//
+//  Created by Sergei Borisov on 17/02/15.
+//  Copyright (c) 2015 Sergei Borisov for 2GIS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ SimpleConverter/SimpleConverterTests/SimpleConverterTests.m
@@ -0,0 +1,40 @@
+//
+//  SimpleConverterTests.m
+//  SimpleConverterTests
+//
+//  Created by Sergei Borisov on 17/02/15.
+//  Copyright (c) 2015 Sergei Borisov for 2GIS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface SimpleConverterTests : XCTestCase
+
+@end
+
+@implementation SimpleConverterTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
